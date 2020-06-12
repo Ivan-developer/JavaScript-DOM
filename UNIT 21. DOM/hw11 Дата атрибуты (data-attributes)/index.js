4ba29f7ab58d1f51b505f8d81d4129ea9e717836
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-12 18:12:59 +0300

    add

---
+++ UNIT 21. DOM/hw11 Дата атрибуты (data-attributes)/index.js
@@ -0,0 +1,4 @@
+const squaredNumbers = () =>
+  Array.from(document.querySelectorAll(".number")).forEach((elem) =>
+    elem.setAttribute("data-squared-number", Math.pow(elem.dataset.number, 2))
+  );
