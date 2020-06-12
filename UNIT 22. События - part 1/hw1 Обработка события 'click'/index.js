46188ed3ef01b45bca46b7e53ca52572f42ff88e
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-12 19:29:53 +0300

    add

---
+++ UNIT 22. События - part 1/hw1 Обработка события 'click'/index.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.create-btn');
+
+const getAlert = () => {
+    alert('done');
+};
+
+btn.addEventListener('click', getAlert);
---
+++ UNIT 22. События - part 1/hw5 Обработка события 'click' - 2/index.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('.single-use-btn');
+
+const getClick = () => {
+    console.log('clicked');
+    btn.removeEventListener('click', getClick);
+
+}
+
+btn.addEventListener('click', getClick);
