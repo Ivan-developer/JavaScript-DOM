6365e821927b31b2a312650e9ae4d1f6cfb2b9e6
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-14 17:55:10 +0300

    add

--- UNIT 23. События - part 2/t2 Делегирование событий/index.js
+++ UNIT 23. События - part 2/t2 Делегирование событий/index.js
@@ -1,56 +1,75 @@
 const generateNumbersRange = (from, to) => {
-    const result = [];
-    for (let i = from; i <= to; i++) {
-        result.push(i);
-
-    }
-    return result;
+  const result = [];
+  for (let i = from; i <= to; i++) {
+    result.push(i);
+  }
+  return result;
 };
 
-const getLineSeats = () => generateNumbersRange(1, 10)
-    .map(seatNumber => `
+// отрисовали
+const getLineSeats = () =>
+  generateNumbersRange(1, 10)
+    .map(
+      (seatNumber) => `
     <div 
         class="sector__seat" 
         data-seat-number="${seatNumber}"
     ></div>
-    `).join('');
+    `
+    )
+    .join("");
 
-
+// отрисовали
 const getSectorLines = () => {
-    const seatsString = getLineSeats();
-    return generateNumbersRange(1, 10)
-        .map(lineNumber => `
+  const seatsString = getLineSeats();
+  return generateNumbersRange(1, 10)
+    .map(
+      (lineNumber) => `
     <div 
         class="sector__line" 
         data-line-number="${lineNumber}"
     >${seatsString}</div>
-    `).join('');
+    `
+    )
+    .join("");
 };
-const arenaElem = document.querySelector('.arena');
+
+const arenaElem = document.querySelector(".arena");
+
+// отрисовали
 const renderArena = () => {
-    const linesString = getSectorLines();
-    const sectorsStrings = generateNumbersRange(1, 3).map(seatNumber => `
+  const linesString = getSectorLines();
+  const sectorsStrings = generateNumbersRange(1, 3)
+    .map(
+      (seatNumber) => `
     <div 
         class="sector" 
         data-sector-number="${seatNumber}"
     >${linesString}</div>
-    `).join('');
-    arenaElem.innerHTML = sectorsStrings;
+    `
+    )
+    .join("");
+  arenaElem.innerHTML = sectorsStrings;
 };
-const onSeatSelect = event => {
-    const isSeat = event.target.classList.contains('sector__seat');
 
-    if (!isSeat) {
-        return;
-    }
+// повесили обработчик arenaElem.addEventListener("click", onSeatSelect);
+const onSeatSelect = (event) => {
+  // принимаем объект event в обработчик
+  // если такой класс есть то это сидения
+  const isSeat = event.target.classList.contains("sector__seat");
 
-    const seatNumber = event.target.dataset.seatNumber;
-    const lineNumber = event.target.parentNode.dataset.lineNumber;
-    const sectorNumber = event.target.closest('.sector').dataset.sectorNumber;
+  if (!isSeat) {
+    return;
+  }
 
-    const board = document.querySelector('.board__selected-seat');
-    board.innerHTML = `S ${seatNumber} - L ${lineNumber} - S ${sectorNumber}`;
-}
-arenaElem.addEventListener('click', onSeatSelect);
+  const seatNumber = event.target.dataset.seatNumber;
+  const lineNumber = event.target.parentNode.dataset.lineNumber;
+  const sectorNumber = event.target.closest(".sector").dataset.sectorNumber;
+
+  const board = document.querySelector(".board__selected-seat");
+  board.innerHTML = `S ${seatNumber} - L ${lineNumber} - S ${sectorNumber}`;
+};
+
+arenaElem.addEventListener("click", onSeatSelect);
 
 renderArena();
