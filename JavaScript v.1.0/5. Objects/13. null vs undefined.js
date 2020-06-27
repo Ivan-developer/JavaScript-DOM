4cfc0d01d3b918c7050b4e2782a2277610a7fd26
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-27 12:38:39 +0300

    add

---
+++ JavaScript v.1.0/5. Objects/13. null vs undefined.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// null - намеренно пусти
+// underfined - ничего небыло присвоенно 
+
+const petya = {
+    name: 'Petya',
+    wife: null,
+};
+
+const tanya = {
+    name: 'Tanya',
+    husband: null,
+};
+
+console.log(
+    tanya.husband,
+    tanya.wife
+)
+
+
