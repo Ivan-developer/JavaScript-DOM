c76b89760e663b551f19057b644072b87f5eccbe
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-27 13:15:05 +0300

    add

---
+++ JavaScript v.1.0/5. Objects/15. function vs object.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Посмотреть расширенно функцию console.dir()
+
+const point = {
+    x: 2,
+    y: 3,
+}
+
+console.log(point)
+
+const sum = function(a,b){
+    return a + b;
+};
+
+console.dir(sum)
