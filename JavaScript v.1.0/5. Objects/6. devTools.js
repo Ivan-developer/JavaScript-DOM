77f67f67fb247ae3b7d78750fe0e9c9503c1765c
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-27 11:33:01 +0300

    add

---
+++ JavaScript v.1.0/5. Objects/6. devTools.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// object in devTools
+// особенности работы в кансоли
+// {count: 0},
+// но когда развернутый объект
+// то в нем count: 1
+
+const user = {
+    count : 0,
+};
+
+console.log(user);
+
+user.count = 1;
