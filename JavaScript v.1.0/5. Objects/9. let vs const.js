b892bec67aa6f70600712ed880b0f44d48bb2abf
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-27 12:02:34 +0300

    add

---
+++ JavaScript v.1.0/5. Objects/9. let vs const.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// объект передается по сылке
+// let / const не влияет
+
+let user = {
+    name: 'Ivan',
+    age: '18',
+};
+
+user.name = 'Tom';
+
+console.log(
+    user.name
+)
+
