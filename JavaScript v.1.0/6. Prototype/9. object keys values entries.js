fa74bd5e5ef5e540e475f5441980d4903c676ebb
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-27 16:58:53 +0300

    add

---
+++ JavaScript v.1.0/6. Prototype/9. object keys values entries.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const father = {
+    lastName: 'Ivanov',
+}
+
+const misha = {
+    name: 'Misha',
+    age: 35,
+    isMarried: true,
+
+    test() {},
+
+    __proto__: father,
+};
+
+for(let key in misha){
+    console.log(key);
+    console.log(misha[key]);
+}
+
+for(let key in misha){
+    if(!misha.hasOwnProperty(key)){
+      continue;
+    } else {
+        console.log(key)
+    }
+}
+
+// свойство из прототипа не включается 
+
+const arr = Object.keys(misha); // возвращает массив 
+const arr2 = Object.values(misha);
+const arr3 = Object.entries(misha);
+
+console.log(
+arr
+)
+
+for(let key of arr2){
+    console.log(key)
+}
+
+// перебор с Деструктуризацией
+
+for(let key of arr3){
+    const [key, value] = key;
+    console.log(key[0], key)
+}
+
+5.30
+
+
+
+
