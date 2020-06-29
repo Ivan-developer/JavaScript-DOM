11001a490cbff5299db71508e985d29d3b79f4b6
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-29 23:19:14 +0300

    add

--- JavaScript v.1.0/10. Closures/9. Замыкания - Фабрика запоминающих устройств.js
+++ JavaScript v.1.0/10. Closures/9. Замыкания - Фабрика запоминающих устройств.js
@@ -4,13 +4,19 @@
 // Задача про строковый буфер
 
 function makeBuffer(){
-
+    let cache = '';
+    return function buffer(str){
+        if(arguments.length > 0){
+            cache += str;
+        }
+        return cache
+    }
 }
 
 let buffer1 = makeBuffer();
 
-buffer1('Замыкание');
-buffer1('Использовать');
+buffer1('Замыкание ');
+buffer1('Использовать ');
 buffer1('Нужно');
 
 let result = buffer1();
