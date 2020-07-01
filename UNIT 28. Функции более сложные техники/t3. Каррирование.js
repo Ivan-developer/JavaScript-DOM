493b80c690b6e67192a48743b17b1b48f77f9708
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-01 15:15:35 +0300

    add

---
+++ UNIT 28. Функции более сложные техники/t3. Каррирование.js
@@ -0,0 +1,13 @@
+//Каррирование
+// 
+
+const sum = (a,b) => a + b;
+
+const sum = a => b => a + b;
+
+const add2 = sum(2);
+const result = add2(3);
+
+console.log(result)
+
+// export { mult, twice, triple };
