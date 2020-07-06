d807c2b1c53fe7cdddc538c5808617690d5a8e2d
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-06 15:33:47 +0300

    add

---
+++ JavaScript v.1.0/14. Events/2. Why not to use attributes for event handlers/index.js
@@ -0,0 +1,25 @@
+// https://developer.mozilla.org/ru/docs/Web/Events
+
+const h1 = document.querySelector('h1');
+
+const input = document.querySelector('input');
+
+const button = document.querySelector('button');
+
+console.log(button.onclick)
+
+// button.onclick = () => {
+//     console.log('Clicked')
+// };
+
+// input.oninput = () => {
+//     console.log(input.value)
+// }
+
+// // h1.onmouseover = () => {
+// //     this.style.color = 'red'
+// // }
+
+// h1.onmouseover = function(){
+//     this.style.color = 'red'
+// }
