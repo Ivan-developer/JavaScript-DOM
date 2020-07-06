388c21cd6fde876c050b82308b7f4613753f6711
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-06 12:18:44 +0300

    add

---
+++ JavaScript v.1.0/13. DOM Basic/5. Find elements/index.js
@@ -0,0 +1,10 @@
+// поиск элементов
+
+document.querySelector('#id');
+document.querySelectorAll('.class');
+document.querySelectorAll('[name="info"]')
+
+document.getElementById('id');
+document.getElementsByTagName('name');
+document.getElementsByClassName('test');
+document.getElementsByName('info')
