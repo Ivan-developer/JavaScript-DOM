848ce8d8db35eed40475b22217510d7327139027
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-06 13:27:53 +0300

    add

---
+++ JavaScript v.1.0/13. DOM Basic/12. Class, className and classList/index.js
@@ -0,0 +1,13 @@
+/*
+Class, className and classList
+
+*/
+
+const h1 = document.querySelector('.page');
+
+h1.classList.add('hello');
+h1.classList.remove('hello');
+
+h1.classList.contains('hello'); // проверяет содержит ли
+
+h1.classList.toggle('hello'); // добавляет ил удаляет
