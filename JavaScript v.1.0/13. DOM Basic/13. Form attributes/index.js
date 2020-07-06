b2855f173968d22cb3fb1282aa5cd64eb040c4ac
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-06 13:50:08 +0300

    add

---
+++ JavaScript v.1.0/13. DOM Basic/13. Form attributes/index.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+console.log(form.action)
+console.log(form.method)
