92ddf6e9b48527de42b7ef98eadab6d3e8bba64c
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-07 12:35:27 +0300

    add

---
+++ JavaScript v.1.0/15. DOM styles and metrics/1. How to add styles to an element/index.js
@@ -0,0 +1,6 @@
+// style это объект с множество свойств
+// пишется camelCase
+
+const list = document.querySelector('ul');
+
+list.style.color = 'red'
---
+++ JavaScript v.1.0/15. DOM styles and metrics/2. getComputedStyle/index.js
@@ -0,0 +1,7 @@
+// getComputedStyle() - метод что бы получить стили из css
+
+const list = document.querySelector('ul');
+
+const style = getComputedStyle(list)
+
+console.log(style)
