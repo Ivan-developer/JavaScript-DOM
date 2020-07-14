29257790a61979921ea9ed6dee131a0c31d03bef
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-14 18:39:56 +0300

    add

--- JavaScript v.3.0/Урок 1. JavaScript. Что такое prototype. JavaScript Prototype/index.js
+++ JavaScript v.3.0/Урок 1. JavaScript. Что такое prototype. JavaScript Prototype/index.js
@@ -1,4 +1,9 @@
 // Урок 1. JavaScript. Что такое prototype. JavaScript Prototype
+// __proto__ сылка на Prototype объекта
+// Prototype это объект, который присутствует у объектов и он вызывается по цепочки сверху вниз
+// в JS всё является объектом
+// __proto__ это объект
+// Prototype это объект, с помощью которого мы расширяем свойства объектов или классов и с помощью его мы може устанавливать наследование
 
 const person = {
     name: 'Ivan',
@@ -11,14 +16,13 @@ const person = {
 person.greet()
 console.log(person)
 
-const person2 = new Object(
-    {
+const person2 = new Object({
     name: 'Ivan',
     age: 18,
     greet: function(){
-        console.log('Greet!'),
+        console.log('Greet!')
     }
-    )
+});
 
 Object.prototype.sayHello = function(){
     console.log('Hello')
@@ -26,6 +30,11 @@ Object.prototype.sayHello = function(){
 
 person2.sayHello()
 
-const lena = Object.create(person2)
+const lena = Object.create(person2);
+lena.name = 'Elena';
+console.log(lena.name);
 
-const str = new String('I am string');
+// Пример у строки есть свои методы
+const str1 = 'I am string';
+console.log(str1.length);
+const str2 = new String('I am string');
--- JavaScript v.3.0/Урок 2. JavaScript. Что такое контекст this. Как работает call, bind, apply/index.js
+++ JavaScript v.3.0/Урок 2. JavaScript. Что такое контекст this. Как работает call, bind, apply/index.js
@@ -1,7 +1,7 @@
 // Урок 2. JavaScript. Что такое контекст this. Как работает call, bind, apply
 
 function hello() {
-    console.log('hello', this) // window глобальный объект
+    console.log('hello', this) // глобальный объект window
 }
 
 const person = {
@@ -11,21 +11,24 @@ const person = {
     sayHelloWinow: hello.bind(),
     loginfo: function(job, phone){
         console.group(`${this.name} info:`)
-        console.log(`Name is ${this.name}`)
+        console.log(`Name is ${this.name}`) // this - объект перед точкой
         console.log(`Age is ${this.age}`)
         console.log(`Job is ${job}`)
         console.log(`Phone is ${phone}`)
         console.groupEnd()
     }
 }
-
+  
 const lena = {
     name: 'Elena',
     age: 23,
 }
+
+// У объекта lena нет функции loginfo, для этого надо закрепить контекст
 // bind , call , apply
-const fnLenaInfoLog = person.loginfo.apply(lena, ['Frontend', '8-999-123-22-23']);
-// fnLenaInfoLog()
+// const fnLenaInfoLog = person.loginfo.apply(lena, ['Frontend', '8-999-123-22-23']);
+const fnLenaInfoLog = person.loginfo.bind(lena, 'Frontend', '8-999-123-22-23');
+fnLenaInfoLog();
 
 //=========================================
 
@@ -37,6 +40,9 @@ const array = [1,2,3,4,5];
 //     })
 // }
 
+// console.log(multiBy(arr,5))
+
+// в объект prototype записали новый метод, который можем юзать с другими масивами, не создавая заново новую фуекцию
 Array.prototype.multBy = function(n){
         return this.map(function(i){
         return i * n;
@@ -44,8 +50,6 @@ Array.prototype.multBy = function(n){
 }
 
 console.log(array.multBy(5))
-
-// console.log(multiBy(arr,5))
 
 
 
